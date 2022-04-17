02218f4ad5b9a1e5f336f250a968453a7eee8523
srabon0 <srabonemam4@gmail.com>
2022-04-17 14:13:45 +0600

    sign up created

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
 
 const Signup = () => {
-    return (
-        <div>
-            this is signup
+  return (
+    <div className="container my-10">
+      <h1 className="text-4xl text-center mb-5 font-semibold underline">Please Sign up</h1>
+      <form className="w-2/4 mx-auto">
+        <div class="form-control mb-5">
+          
+          <label class="input-group">
+            <span>Name</span>
+            <input
+              type="text"
+              placeholder="your name here"
+              class="input input-bordered w-full"
+            />
+          </label>
         </div>
-    );
+        <div class="form-control mb-5">
+          <label class="input-group">
+            <span>Email</span>
+            <input
+              type="text"
+              placeholder="Enter you email here"
+              class="input input-bordered w-full"
+            />
+          </label>
+        </div>
+        <div class="form-control mb-5">
+          <label class="input-group">
+            <span>Password</span>
+            <input
+              type="passwprd"
+              placeholder="Enter your password here"
+              class="input input-bordered w-full"
+            />
+          </label>
+        </div>
+
+        <button class="btn sm:btn-sm md:btn-md btn-wide">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
